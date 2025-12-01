41e4e3baa4d1c7db25eec15018b950870d240f74
sinawecuba <sinawesome@gmail.com>
2025-12-01 09:59:07 +0200

    Add files via upload

---
+++ Classwork/unit_0/lesson1/lesson1.js
@@ -0,0 +1,18 @@
+3+3
+3/0
+console.log("Hello, Universe!");
+ let name = "Jon Wexler";
+console.log(name);
+3+3
+3/0
+ console.log("Hello, Universe!");
+console.log(name);
+.class Goat {
+class Goat {
+ eat(foodType) {
+    console.log(`I love eating ${foodType}`);
+  }
+ }
+let billy = new Goat();
+billy.eat('tin cans");
+ billy.eat("tin cans");
